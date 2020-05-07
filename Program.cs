efa761321d7f2523b090fe42dac44ae2d358abac
MashaKarabinovich <64698383+MashaKarabinovich@users.noreply.github.com>
2020-05-07 16:11:44 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace palindromeRecursion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+              string userInput;
+            Console.WriteLine("Please enter a word: ");
+            userInput = Console.ReadLine();
+            Console.WriteLine(palindrome(userInput));
+            Console.ReadLine();
+
+        }
+
+        public static bool palindrome(string word)
+        {
+
+            if (word.Length <= 1)
+            {
+                return true;
+            }
+
+            if (word[0] != word[word.Length - 1])
+            {
+                return false;
+            }
+            return palindrome(word.Substring(1, word.Length - 2));
+
+        }
         }
     }
 }
